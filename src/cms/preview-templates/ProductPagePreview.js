fe65ea18c4e44f1ce47bfa0cad15a37d8527e2a7
Miguel Martin <miguel.martin@unitedcuisines.com>
2020-06-08 14:06:08 +0200

    Change pricing to areas

--- src/cms/preview-templates/ProductPagePreview.js
+++ src/cms/preview-templates/ProductPagePreview.js
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types'
 import { ProductPageTemplate } from '../../templates/product-page'
 
 const ProductPagePreview = ({ entry, getAsset }) => {
-  const entryPricingPlans = entry.getIn(['data', 'pricing'])
-  const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
+  const entryPricingPlans = entry.getIn(['data', 'areas'])
+  const areas = entryPricingPlans ? entryPricingPlans.toJS() : []
 
   return (
     <ProductPageTemplate
       image={getAsset(entry.getIn(['data', 'image']))}
       heading={entry.getIn(['data', 'heading'])}
       description={entry.getIn(['data', 'description'])}
-      pricing={pricingPlans}
+      areas={areas}
     />
   )
 }
