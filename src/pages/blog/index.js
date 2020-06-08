607c6136154c5dffead1bb08bc75685dae41449d
Miguel Martin <miguel.martin@unitedcuisines.com>
2020-06-08 20:21:25 +0200

    Change blog-hero

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -10,10 +10,10 @@ export default class BlogIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
+            backgroundImage: `url('/img/blog-hero.jpg')`,
           }}
         >
-          <h1
+{/*           <h1
             className="has-text-weight-bold is-size-1"
             style={{
               boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
@@ -23,7 +23,7 @@ export default class BlogIndexPage extends React.Component {
             }}
           >
             Latest Stories
-          </h1>
+          </h1 */}>
         </div>
         <section className="section">
           <div className="container">
